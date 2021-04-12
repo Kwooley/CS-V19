b5f5c0c656dc859cfe7088768c3f9b75f9848df3
Kayw Lee <kwooler@gmail.com>
2021-04-11 19:03:28 -0700

    Ex1

---
+++ Ex1data.asm
@@ -0,0 +1,12 @@
+.ORIG x3100	; start the data at location x3100
+    .FILL #1	; the value of the first data element
+    .FILL #2	
+    .FILL #3	
+    .FILL #4	
+    .FILL #5	
+    .FILL #6	
+    .FILL #7	
+    .FILL #8	
+    .FILL #9
+    .FILL #10
+.END	
