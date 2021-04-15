a1b0acb64b23e3e3facd2ac132c03e229e26349c
Kayw Lee <kwooler@gmail.com>
2021-04-14 20:10:20 -0700

    6223

---
+++ 6223TheFives-data.asm
@@ -0,0 +1,12 @@
+.Orig x3100
+    .FILL   #9
+    .FILL   #7
+    .FILL   #32
+    .FILL   #0
+    .FILL   #-8
+    .FILL   #19
+    .FILL   #6
+    .FILL   #13
+    .FILL   #5
+    .FILL   #61
+.End
